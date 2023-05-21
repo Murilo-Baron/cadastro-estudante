ee833133d58336fd26699054cca4c6e71d02c04f
RENAN-PECANHA <renan.pecanha951@gmail.com>
2023-05-21 14:01:48 -0300

    component e imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdastroEstudanteComponent } from './adastro-estudante/adastro-estudante.component';
+import { CadastroEstudanteComponent } from './cadastro-estudante/cadastro-estudante.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdastroEstudanteComponent,
+    CadastroEstudanteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cadastro-estudante/cadastro-estudante.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-estudante',
+  templateUrl: './cadastro-estudante.component.html',
+  styleUrls: ['./cadastro-estudante.component.css']
+})
+export class CadastroEstudanteComponent {
+
+}
