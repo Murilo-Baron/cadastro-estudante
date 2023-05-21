ab2cf26d28efbd13bcb3241449dc9ba3f6300ed9
RENAN-PECANHA <renan.pecanha951@gmail.com>
2023-05-21 14:26:53 -0300

    bootstrap e json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdastroEstudanteComponent } from './adastro-estudante/adastro-estudante.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CadastroEstudanteComponent } from './cadastro-estudante/cadastro-estudante.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdastroEstudanteComponent,
     CadastroEstudanteComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot
+    ([
+    { path: 'cadastro', component: CadastroEstudanteComponent }
+    ]), // Adicione esta linha
     AppRoutingModule
   ],
   providers: [],
--- src/app/cadastro-estudante/cadastro-estudante.component.ts
+++ src/app/cadastro-estudante/cadastro-estudante.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro-estudante',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro-estudante.component.css']
 })
 export class CadastroEstudanteComponent {
+  estudanteForm: FormGroup;
+  estudantes: any[] = [];
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.estudanteForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      idade: ['', Validators.required],
+      curso: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required]
+    });
+  }
+
+  cadastrarEstudante() {
+    if (this.estudanteForm.invalid) {
+      return;
+    }
+
+    const estudante = this.estudanteForm.value;
+
+    this.http.post('http://localhost:3000/estudantes', estudante)
+      .subscribe(
+        response => {
+          console.log('Estudante cadastrado com sucesso:', response);
+          this.estudanteForm.reset();
+        },
+        error => {
+          console.error('Erro ao cadastrar o estudante:', error);
+        }
+      );
+  }
+
+  carregarEstudantes() {
+    this.http.get('http://localhost:3000/estudantes')
+      .subscribe(
+        (estudantes: any) => {
+          this.estudantes = estudantes;
+        },
+        error => {
+          console.error('Erro ao carregar os estudantes:', error);
+        }
+      );
+  }
 }
